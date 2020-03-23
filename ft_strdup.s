ab39a5030a9ac5cc4fd67b804448dc3112170b99
Mohamed Ait Si Mhand <hacker.simou2013@gmail.com>
2020-03-23 01:27:28 +0100

    last commit

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,5 +1,24 @@
-global _ft_strdup
-extern malloc
 section .text
+	global _ft_strdup
+	extern _malloc
+	extern _ft_strcpy
+	extern _ft_strlen
 _ft_strdup:
-	
+	push rdi
+	call _ft_strlen
+	pop rdi
+	mov r13, rax
+	inc r13
+	push rdi
+	mov rdi, r13
+	call _malloc
+	pop rdi
+	cmp rax, 0
+	je _end
+
+	mov rsi, rdi
+	mov rdi, rax
+	call _ft_strcpy
+	ret
+_end:
+	ret
